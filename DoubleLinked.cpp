1e5364039c9455763c4800055fe66fa912a6058a
Furkan-Code <111159462+Furkan-Code@users.noreply.github.com>
2022-09-18 01:24:32 +0300

    Create DoubleLinked.cpp

---
+++ DoubleLinked.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+struct Node
+{
+  struct Node *prev;
+  int data;
+  struct Node *next;
+}*first = NULL;
+void create(int A[], int n)
+{
+  struct Node *t, *last;
+  int i;
+  first =(struct Node*) malloc(sizeof(struct Node));
+  first->data=A[0];
+  first->prev=first->next=NULL;
+  last=first;
+  for(i=1; i <n; i++)
+  {
+    t=(struct Node *)malloc(sizeof(struct Node));
+    t->data=A[i];
+    t->next=last->next;
+    t->prev=last;
+    last->next=t;
+    last=t;
+  }
+}
+void Display(struct Node *p)
+{
+  while(p)
+  {
+    printf("%d ", p->data);
+    p=p->next;
+  }
+  printf("\n");
+}
+
+int main() {
+  int A[]={10,20,30,40,50};
+  create(A,5);
+  
+  Display(first);
+
+  return 0;
+}
